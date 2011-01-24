d8543bbdc4609bbb3b23feb1987da10a38a2ddef
Jean Privat <jean@pryen.org>
2011-01-24 16:31:55 -0500

    parser: use C tables in lexer
    The new module parser/tables contains the tables used during the lexical
    analysis.
    Therefore, we do not need to have big tables in the Nit code for the lexer.
    
    During the conversion, the handling of states (aka lexical contexts)
    is removed since it is not used.
    
    Signed-off-by: Jean Privat <jean@pryen.org>

---
+++ src/parser/tables_nit.h
@@ -0,0 +1,20 @@
+#ifndef TABLE_NIT_H
+#define TABLE_NIT_H
+/* This file is part of NIT ( http://www.nitlanguage.org ).
+ *
+ * This file is free software, which comes along with NIT.  This software is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without  even  the implied warranty of  MERCHANTABILITY or  FITNESS FOR A
+ * PARTICULAR PURPOSE.  You can modify it is you want,  provided this header
+ * is kept unaltered, and a notification of the changes is added.
+ * You  are  allowed  to  redistribute it and sell it, alone or is a part of
+ * another product.
+ */
+
+extern const int* const lexer_goto_table[];
+extern const int lexer_accept_table[];
+
+#define lexer_goto(o,i,j) (lexer_goto_table[(i)][(j)])
+#define lexer_accept(o,i) (lexer_accept_table[(i)])
+
+#endif
