45b4227e6ccd10c55466effbf66186dea596d23c
Jean Privat <jean@pryen.org>
2013-05-25 05:46:24 -0400

    test: add testfull.sh
    Signed-off-by: Jean Privat <jean@pryen.org>

---
+++ tests/testfull.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./tests.sh "$@" *.nit ../examples/*.nit ../examples/leapfrog/leapfrog.nit ../examples/shoot/shoot_logic.nit ../lib/*.nit ../src/nitdoc.nit ../src/nits.nit ../src/nitc.nit ../src/test_parser.nit ../src/nit.nit ../src/nitmetrics.nit ../src/nitg.nit
