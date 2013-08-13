3a05b68e761bf16bb7e78ae0e7b11d1d65c8393d
Lucas Bajolet <lucas.bajolet@hotmail.com>
2013-08-13 13:59:46 -0400

    stdlib: Strings, optimized to_s for Int.
    Signed-off-by: Lucas Bajolet <lucas.bajolet@hotmail.com>

---
+++ lib/standard/string_nit.h
@@ -0,0 +1,18 @@
+#ifndef __STRING_NIT_H
+#define __STRING_NIT_H
+#include <stdio.h>
+#include <stdlib.h>
+/* This file is part of NIT ( http://www.nitlanguage.org ).
+ *
+ * This file is free software, which comes along with NIT.  This software is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without  even  the implied warranty of  MERCHANTABILITY or  FITNESS FOR A
+ * PARTICULAR PURPOSE.  You can modify it is you want,  provided this header
+ * is kept unaltered, and a notification of the changes is added.
+ * You  are  allowed  to  redistribute it and sell it, alone or is a part of
+ * another product.
+ */
+
+char* native_int_to_s(int recv, int len);
+
+#endif
