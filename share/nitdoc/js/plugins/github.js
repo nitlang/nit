ae777610fa9d3af896df219141dc3c20b8395008
Alexandre Terrasa <alexandre@moz-code.org>
2014-02-15 00:25:18 -0500

    nitdoc: rewrite GitHub plugin
    * move icon on the left top corner
    * merge github and ui plugins
    * merge comment box and commit box
    * commit message and signoff are displayed under the modifier comment
    * no more modal for commit sign off
    * also edited styles
    
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- share/nitdoc/js/plugins/github.js
+++ share/nitdoc/js/plugins/github.js
@@ -24,70 +24,447 @@
 define([
 	"jquery",
 	"base64",
-	"plugins/github/ui",
+	"plugins/github/github_api",
 	"plugins/github/loginbox",
 	"plugins/github/modalbox",
-], function($, Base64, UI, LoginBox, ModalBox) {
-	// Load GitHub UI
+	"plugins/github/commentbox",
+	"Markdown.Converter",
+], function($, Base64, GithubAPI, LoginBox, ModalBox, CommentBox) {
+	var GithubUser = function(login, password, repo, branch) {
+		this.login = login;
+		this.password = password;
+		this.repo = repo;
+		this.auth = "Basic " +  Base64.encode(login + ':' + password);
+		this.branch = branch;
+	}
+
+	var GithubUI = {
+		init: function(upstream, basesha1) {
+			console.info("Github plugin: init GitHub module (upstream: "+ upstream +", base: " + basesha1 + ")");
+			this.origin = this._parseUpstream(upstream);
+			// Add github menu
+			$("nav.main ul").append(
+				$("<li/>")
+				.attr("id", "nitdoc-github-li")
+				.loginbox()
+				.loginbox("displayLogin")
+				.bind("loginbox_logoff", function() {
+					GithubUI.disactivate();
+				})
+				.bind("loginbox_login", function(event, infos) {
+					GithubUI._tryLoginFromCredentials(infos);
+				})
+			);
+			// check local session
+			this._tryLoginFromLocalSession();
+		},
+
+		activate: function(user, origin) {
+			this.openedComments = 0;
+			this._saveSession(user);
+			$("#nitdoc-github-li").loginbox("displayLogout", origin, user);
+			this._attachCommentBoxes();
+			this._reloadComments();
+
+			// Prevent page unload if there is comments in editing mode
+			$(window).on('beforeunload', function() {
+				if(GithubUI.openedComments > 0){
+					return "There is uncommited modified comments. Are you sure you want to leave this page?";
+				}
+			});
+		},
+
+		disactivate: function() {
+			if(this.openedComments > 0){
+				if(!confirm('There is uncommited modified comments. Are you sure you want to leave this page?')) {
+					return false;
+				}
+			}
+
+			localStorage.clear();
+			$("#nitdoc-github-li").loginbox("toggle");
+			$("#nitdoc-github-li").loginbox("displayLogin");
+			$(window).unbind('beforeunload');
+			//window.location.reload();
+		},
+
+		/* login */
+
+		_checkLoginInfos: function(infos) {
+			if(!infos.login || !infos.password || !infos.repo || !infos.branch) {
+				ModalBox.open(
+					"Sign in error",
+					"Please enter your GitHub username, password, repository and branch.",
+					true
+				);
+				return false;
+			} else {
+				return true;
+			}
+		},
+
+		_tryLoginFromCredentials: function(infos) {
+			if(this._checkLoginInfos(infos)) {
+				var isok = this._tryLogin(infos.login, infos.password, infos.repo, infos.branch);
+				if(isok === true) {
+					this.activate(this.user, this.origin);
+				} else {
+					if(isok == "error:login") {
+						ModalBox.open(
+							"Sign in error",
+							"The username, password, repo or branch you entered is incorrect.",
+							true
+						);
+					} else if(isok == "error:sha") {
+						ModalBox.open(
+							"Base commit not found",
+							"The provided Github repository must contain the base commit '" + UI.origin.sha + "'",
+							true
+						);
+					} else if(isok == "error:profile") {
+						ModalBox.open(
+							"Incomplete Github profile",
+							"Please set your public name and email in your " +
+							"<a href='https://github.com/settings/profile'>GitHub profile</a>." +
+							"<br/><br/>Your public profile informations are used to sign-off your commits.",
+							true
+						);
+					}
+				}
+			}
+		},
+
+		_tryLoginFromLocalSession: function() {
+			if(localStorage.user) {
+				var session = JSON.parse(localStorage.user);
+				var isok = this._tryLogin(
+					session.login,
+					Base64.decode(session.password),
+					session.repo,
+					session.branch
+				);
+				if(isok === true) {
+					this.activate(this.user, this.origin);
+				} else {
+					console.debug("Github plugin: Session found but authentification failed");
+					localStorage.clear();
+				}
+			} else {
+				console.debug("Github plugin: No session found");
+			}
+		},
+
+		_tryLogin: function(login, password, repo, branch) {
+			var tmpUser = new GithubUser(login, password, repo, branch);
+			if(!GithubAPI.login(tmpUser)) {
+				return "error:login";
+			}
+			if(!tmpUser.infos.name || !tmpUser.infos.email) {
+				return "error:profile";
+			}
+			var commit = GithubAPI.getCommit(tmpUser, this.origin.sha);
+			if(!commit || !commit.sha) {
+				return "error:sha";
+			}
+			this.user = tmpUser;
+			return true;
+		},
+
+		_saveSession: function(user) {
+			localStorage.user = JSON.stringify({
+				login: user.login,
+				password: Base64.encode(user.password),
+				repo: user.repo,
+				branch: user.branch,
+			});
+			// check local storage synchro with branch
+			if(localStorage.base != this.origin.sha) {
+				console.log("Base changed: cleaned cache");
+				localStorage.requests = "[]";
+				localStorage.base = this.origin.sha;
+			}
+		},
+
+		/* html decoration */
+
+		// Attach edit button on each comment
+		_attachCommentBoxes: function() {
+			$("textarea.baseComment").each(function() {
+				$(this).commentbox();
+
+				var isNew = false;
+				if(!$(this).val()) {
+					isNew = true;
+					$(this).nextAll(".info:first").find(".noComment").hide()
+					$(this).nextAll(".info:first").before(
+						$("<div/>")
+						.hide()
+						.addClass("comment")
+						.append(
+							$("<div/>").addClass("nitdoc")
+						)
+					)
+				}
+
+				$(this).nextAll(".info:first").prepend(
+					$("<a/>")
+					.addClass("nitdoc-github-editComment")
+					.css("cursor", "pointer")
+					.text((isNew ? "add" : "edit") + " comment")
+					.click($.proxy(GithubUI._openCommentBox, GithubUI, null, $(this)))
+					.after(" for ")
+				)
+
+				$(this).bind("commentbox_commit", function(event, data) {
+					GithubUI._saveChanges(data);
+					$(this).commentbox("close");
+					GithubUI._reloadComments();
+				})
+				.bind("commentbox_preview", function(event, data) {
+					var converter = new Markdown.Converter()
+					var html = converter.makeHtml(data.value);
+					ModalBox.open("Preview", html, false);
+				})
+				.bind("commentbox_open", function(event, data) {
+					GithubUI.openedComments++;
+					$(this).nextAll(".comment").hide();
+				})
+				.bind("commentbox_close", function(event, data) {
+					GithubUI.openedComments--;
+					$(this).nextAll(".comment").show();
+				});
+			});
+		},
+
+		// reload comments from saved pull request
+		_reloadComments: function() {
+			if(!localStorage.requests){ return; }
+			$("p.pullRequest").remove();
+			var converter = new Markdown.Converter();
+			var requests = JSON.parse(localStorage.requests);
+			// Look for modified comments in page
+			for(i in requests) {
+				if(!requests[i]) { continue; }
+				var request = requests[i];
+				$("textarea[data-comment-location=\"" + request.location + "\"]").each(function () {
+					if(request.isClosed) {
+						var oldComment = Base64.decode(request.oldComment);
+						var htmlComment = converter.makeHtml(oldComment);
+						$(this).val(oldComment);
+						if(!$(this).val()) {
+							$(this).nextAll("div.comment:first").hide();
+						} else {
+							$(this).nextAll("div.comment:first").show();
+						}
+						$(this).nextAll("div.comment").find("div.nitdoc").empty().html(htmlComment);
+						$(this).nextAll("p.info").find("a.nitdoc-github-editComment").show();
+					} else {
+						var newComment = Base64.decode(request.comment);
+						var htmlComment = converter.makeHtml(newComment);
+						$(this).val(newComment);
+						if(!$(this).val()) {
+							$(this).nextAll("div.comment:first").hide();
+						} else {
+							$(this).nextAll("div.comment:first").show();
+						}
+						$(this).nextAll("div.comment").find("div.nitdoc").empty().html(htmlComment);
+						GithubUI._addPullRequestLink($(this), request);
+						$(this).nextAll("p.info").find("a.nitdoc-github-editComment").hide();
+					}
+				});
+			}
+		},
+
+		_addPullRequestLink: function(baseArea, request) {
+			baseArea.nextAll("p.info").before(
+				$("<p/>")
+				.addClass("pullRequest inheritance")
+				.text("comment modified in ")
+				.append(
+					$("<a/>")
+					.attr({
+						href: request.request.html_url,
+						title: "Review on GitHub"
+					})
+					.text("pull request #" + request.request.number)
+				)
+				.append(" ")
+				.append(
+					$("<a/>")
+					.data("pullrequest-number", request.request.number)
+					.addClass("nitdoc-github-update")
+					.text("update")
+					.click($.proxy(GithubUI._doUpdateRequest, GithubUI, null, baseArea, request))
+				)
+				.append(" ")
+				.append(
+					$("<a/>")
+					.data("pullrequest-number", request.request.number)
+					.addClass("nitdoc-github-cancel")
+					.text("cancel")
+					.click($.proxy(GithubUI._doCancelRequest, GithubUI, null, baseArea, request))
+				)
+			);
+		},
+
+		/* github calls */
+
+		_saveChanges: function(edit) {
+			// if pull request update close existing pull request for the comment
+			if(edit.requestID) {
+				this._closePullRequest(edit.requestID);
+			}
+			edit.oldContent = this._getFileContent(edit.location.path);
+			edit.newContent = this._mergeComment(edit.oldContent, edit.newComment, edit.location);
+			edit.request = this._pushChanges(edit)
+			if(!edit.request) {
+				ModalBox.open("Unable to commit changes!", response, true);
+				return;
+			}
+			this._saveRequest(edit);
+		},
+
+		// save pull request in local storage
+		_saveRequest: function(edit) {
+			var requests = {};
+			if(localStorage.requests) {requests = JSON.parse(localStorage.requests)}
+			requests[edit.request.number] = {
+				request: edit.request,
+				location: edit.location.origin,
+				comment: Base64.encode(edit.newComment),
+				oldComment: Base64.encode(edit.oldComment)
+			};
+			localStorage.requests = JSON.stringify(requests);
+		},
+
+		/*
+		   Creating a new pull request with the new comment take 5 steps:
+			1. get the base tree from latest commit
+
+			2. create a new blob with updated file content
+			3. post a new tree from base tree and blob
+			4. post the new commit with new tree
+			5. create the pull request
+		*/
+		_pushChanges: function(edit) {
+			var baseTree = GithubAPI.getTree(this.user, this.origin.sha);
+			if(!baseTree.sha) {
+				ModalBox.open("Unable to locate base tree!", baseTree.status + ": " + baseTree.statusText, true);
+				return false;
+			}
+			console.log("Base tree: " + baseTree.url);
+			var newBlob = GithubAPI.createBlob(this.user, edit.newContent);
+			if(!newBlob.sha) {
+				ModalBox.open("Unable to create new blob!", newBlob.status + ": " + newBlob.statusText, true);
+				return false;
+			}
+			console.log("New blob: " + newBlob.url);
+			var newTree = GithubAPI.createTree(this.user, baseTree, edit.location.path, newBlob);
+			if(!newTree.sha) {
+				ModalBox.open("Unable to create new tree!", newTree.status + ": " + newTree.statusText, true);
+				return false;
+			}
+			console.log("New tree: " + newTree.url);
+			var newCommit = GithubAPI.createCommit(this.user, edit.message, baseTree.sha, newTree);
+			if(!newCommit.sha) {
+				ModalBox.open("Unable to create new commit!", newCommit.status + ": " + newCommit.statusText, true);
+				return false;
+			}
+			console.log("New commit: " + newCommit.url);
+			var pullRequest = GithubAPI.createPullRequest(this.user, edit.title, "Pull request from Nitdoc", this.origin, newCommit.sha);
+			if(!pullRequest.number) {
+				ModalBox.open("Unable to create pull request!", pullRequest.status + ": " + pullRequest.statusText, true);
+				return false;
+			}
+			console.log("New pull request: " + pullRequest.url);
+			return pullRequest;
+		},
+
+		// close previously opened pull request
+		_closePullRequest: function(number) {
+			var requests = JSON.parse(localStorage.requests);
+			if(!requests[number]) {
+				ModalBox.open("Unable to close pull request!", "Pull request " + number + "not found", true);
+				return false;
+			}
+			// close pull request
+			var res = GithubAPI.updatePullRequest(this.user, "Closed from Nitdoc", "", "closed", requests[number].request);
+			if(!res.id) {
+				ModalBox.open("Unable to close pull request!", res.status + ": " + res.statusText, true);
+				return false;
+			}
+			// update in localstorage
+			requests[number].isClosed = true;
+			localStorage.requests = JSON.stringify(requests);
+		},
+
+		/* internals */
+
+		_parseUpstream: function(upstream) {
+			var parts = upstream.split(":");
+			return {
+				user: parts[0],
+				repo: parts[1],
+				branch: parts[2],
+				sha: basesha1
+			};
+		},
+
+		_getFileContent: function(githubUrl) {
+			var origFile = GithubAPI.getFile(this.user, githubUrl);
+			if(!origFile.content) {
+				ModalBox.open("Unable to locate source file!", origFile.status + ": " + origFile.statusText, true);
+				return;
+			}
+			var base64Content = origFile.content.substring(0, origFile.content.length - 1)
+			return Base64.decode(base64Content);
+		},
+
+		_mergeComment: function(fileContent, comment, location) {
+			// replace comment in file content
+			var res = new String();
+			var lines = fileContent.split("\n");
+			// copy lines fron 0 to lstart
+			for(var i = 0; i < location.lstart - 1; i++) {
+				res += lines[i] + "\n";
+			}
+			// set comment
+			if(comment && comment != "") {
+				var commentLines = comment.split("\n");
+				for(var i = 0; i < commentLines.length; i++) {
+					var line = commentLines[i];
+					var tab = location.tabpos > 1 ? "\t" : "";
+					res += tab + (line.length > 0 ? "# " : "#") + line + "\n";
+				}
+			}
+			// copy lines fron lend to end
+			for(var i = location.lend - 1; i < lines.length; i++) {
+				res += lines[i];
+				if(i < lines.length - 1) { res += "\n"; }
+			}
+			return res;
+		},
+
+		/* events */
+
+		_openCommentBox: function(event, baseArea) {
+			baseArea.commentbox("open", this.user);
+		},
+
+		_doCancelRequest: function(event, baseArea, request) {
+			this._closePullRequest(request.request.number);
+			this._reloadComments();
+		},
+
+		_doUpdateRequest: function(event, baseArea, request) {
+			baseArea.commentbox("open", this.user, request.request.number);
+		}
+	}
+
+	// Get github plugin data
 	var upstream = $("body").attr("data-github-upstream");
 	var basesha1 = $("body").attr("data-github-base-sha1");
 	if(upstream && basesha1) {
-		console.info("Github plugin: init GitHub module (upstream: "+ upstream +", base: " + basesha1 + ")");
-
-		$("nav.main ul").append(
-			$("<li/>").attr("id", "nitdoc-github-li")
-		);
-		$("#nitdoc-github-li")
-		.loginbox()
-		.bind( "loginbox_logoff", function() {
-			UI.disactivate();
-			$("#nitdoc-github-li").loginbox("toggle");
-		})
-		.bind( "loginbox_login", function(event, infos) {
-			if(!infos.login || !infos.password || !infos.repo || !infos.branch) {
-				ModalBox.open("Sign in error", "Please enter your GitHub username, password, repository and branch.", true);
-			} else {
-				var user = UI.tryLogin(infos.login, infos.password, infos.repo, infos.branch);
-				if(user == "error:login") {
-					ModalBox.open("Sign in error", "The username, password, repo or branch you entered is incorrect.", true);
-				} else if(user == "error:sha") {
-					ModalBox.open("Base commit not found", "The provided GitHub repository must contains the base commit '" + UI.origin.sha + "'", true);
-				} else if(user == "error:profile") {
-					ModalBox.open("Incomplete GitHub profile", "Please set your public name and email in your <a href='https://github.com/settings/profile'>GitHub profile</a>.<br/><br/>Your public profile informations are used to sign-off your commits.", true);
-				} else {
-					UI.activate(user);
-					$("#nitdoc-github-li").loginbox("displayLogout", UI.origin, UI.user);
-				}
-			}
-		});
-
-		// parse origin
-		var parts = upstream.split(":");
-		UI.origin = {
-			user: parts[0],
-			repo: parts[1],
-			branch: parts[2],
-			sha: basesha1
-		};
-
-		// check local session
-		if(localStorage.user) {
-
-			var session = JSON.parse(localStorage.user);
-			UI.user = UI.tryLogin(session.login, Base64.decode(session.password), session.repo, session.branch);
-			if(!UI.user.login) {
-				console.debug("Github plugin: Session found but authentification failed");
-				localStorage.clear();
-			}
-
-			// activate ui
-			if(UI.user && UI.user.login) {
-				$("#nitdoc-github-li").loginbox("displayLogout", UI.origin, UI.user);
-				UI.activate(UI.user);
-			}
-		} else {
-			console.debug("Github plugin: No session found");
-			$("#nitdoc-github-li").loginbox("displayLogin");
-		}
+		GithubUI.init(upstream, basesha1);
 	}
 });
--- share/nitdoc/js/plugins/github/commentbox.js
+++ share/nitdoc/js/plugins/github/commentbox.js
@@ -20,59 +20,136 @@ define([
 	"jquery",
 	"jQueryUI"
 ], function($) {
+	var Location = function(location) {
+		var parts = location.split(":");
+		this.origin = location;
+		this.path = parts[0];
+		this.lstart = parseInt(parts[1].split("--")[0].split(",")[0]);
+		this.tabpos = parseInt(parts[1].split("--")[0].split(",")[1]);
+		this.lend = parseInt(parts[1].split("--")[1].split(",")[0]);
+		this.toString = function() {
+			return this.path + ":" + this.lstart + "," + this.tabpos + "--" + this.lend + ",0";
+		}
+	}
+
 	$.widget("nitdoc.commentbox", {
 
 		options: {
 			previewTxt: "preview",
-			commitTxt: "Commit...",
-			cancelTxt: "Cancel"
+			commitTxt: "Commit",
+			cancelTxt: "Cancel",
+			commentboxTitle: "Edit comment",
+			messageTxt: "Commit message"
 		},
 
 		_create: function() {
+			this._id = $(".nitdoc-github-commentbox").length
+			this._oldComment = this.element.val();
+			this._namespace = this.element.data("comment-namespace");
+			this._location = new Location(this.element.data("comment-location"));
 			this.commentBox = $("<div/>")
+			.hide()
 			.addClass("nitdoc-github-commentbox")
 			.append(
-				$("<textarea/>")
-				.addClass("nitdoc-github-commentarea")
-				.keyup($.proxy(this._doKeyUp, this))
-				.keydown($.proxy(this._doKeyDown, this))
+				$("<h3/>")
+				.text(this.options.commentboxTitle)
 			)
 			.append(
-				$("<a/>")
-				.addClass("nitdoc-github-preview")
-				.html(this.options.previewTxt)
-				.click($.proxy(this._doPreviewClick, this))
+				$("<dl/>")
+				.addClass("nitdoc-github-commentbox-fields")
+				.append(
+					$("<dd/>")
+					.append(
+						$("<textarea/>")
+						.attr("id", "nitdoc-github-commentbox-comment" + this._id)
+						.addClass("nitdoc-github-commentarea")
+						.keyup($.proxy(this._doKeyUp, this))
+						.keydown($.proxy(this._doKeyDown, this))
+					)
+				)
+				.append(
+					$("<dt/>")
+					.append(
+						$("<label/>")
+						.attr("for", "nitdoc-github-commentbox-message" + this._id)
+						.text(this.options.messageTxt + ":")
+					)
+				)
+				.append(
+					$("<dd/>")
+					.append(
+						$("<textarea/>")
+						.attr("id", "nitdoc-github-commentbox-message" + this._id)
+						.keyup($.proxy(this._doKeyUp, this))
+						.keydown($.proxy(this._doKeyDown, this))
+					)
+				)
+				.append(
+					$("<dt/>")
+					.append(
+						$("<input/>")
+						.attr({
+							id: "nitdoc-github-commentbox-signedoff" + this._id,
+							type: "checkbox"
+						})
+						.change($.proxy(this._doSignedChange, this))
+					)
+					.append(
+						$("<label/>")
+						.attr({
+							"id": "nitdoc-github-commentbox-signedoff-label" + this._id,
+							"for": "nitdoc-github-commentbox-signedoff" + this._id
+						})
+					)
+				)
 			)
-			.append(
-				$("<button/>")
-				.addClass("nitdoc-github-button")
-				.addClass("nitdoc-github-commit")
-				.html(this.options.commitTxt)
-				.click($.proxy(this._doCommitClick, this))
-			)
-			.append(
-				$("<button/>")
-				.addClass("nitdoc-github-button")
-				.addClass("nitdoc-github-cancel")
-				.html(this.options.cancelTxt)
-				.click($.proxy(this._doCancelClick, this))
-			);
-
+			this._buildButtonBar();
 			this.element.after(this.commentBox);
+		},
+
+		_buildButtonBar: function() {
+			this.commentBox.append(
+				$("<div/>")
+				.addClass("nitdoc-github-commentbox-buttons")
+				.append(
+					$("<a/>")
+					.addClass("nitdoc-github-preview")
+					.html(this.options.previewTxt)
+					.click($.proxy(this._doPreviewClick, this))
+				)
+				.append(
+					$("<button/>")
+					.addClass("nitdoc-github-button")
+					.addClass("nitdoc-github-commit")
+					.attr("disabled", "disabled")
+					.html(this.options.commitTxt)
+					.click($.proxy(this._doCommitClick, this))
+				)
+				.append(
+					$("<button/>")
+					.addClass("nitdoc-github-button")
+					.addClass("nitdoc-github-cancel")
+					.html(this.options.cancelTxt)
+					.click($.proxy(this._doCancelClick, this))
+				)
+			);
 		},
 
 		/* public actions */
 
-		open: function(value) {
-			this._originalValue = value;
-			this._setValue(value);
-			this._autosize();
+		open: function(user, requestID) {
+			this._requestID = requestID;
+			var value = this.element.val();
+			var isNew = !value;
+			var message = "doc: " + (isNew ? "added" : "modified") + " comment for " + this._namespace;
+			this._setMessage(message);
+			this._setSignedOff("Signed-off-by: " + user.signedOff);
+			this._setComment(value);
 			this.commentBox.show();
-			var cbw = this.commentBox.innerWidth();
-			var taw = this._getArea().outerWidth();
-			this._getArea().width(cbw - (taw - cbw));
-			this.commentBox.find("textarea").trigger("keyup");
-			this.commentBox.find("textarea").focus();
+			this.commentBox.find("textarea").width(this.commentBox.innerWidth() - 45)
+			$("#nitdoc-github-commentbox-comment" + this._id).focus();
+			$("#nitdoc-github-commentbox-comment" + this._id).trigger("keyup");
+			$("#nitdoc-github-commentbox-message" + this._id).trigger("keyup");
 			this._trigger("_open", null, {commentBox: this});
 		},
 
@@ -83,45 +160,65 @@ define([
 
 		/* internals */
 
-		_autosize: function() {
-			this._getArea().height(this._getArea().val().split(/\r|\n/).length * 16);
+		_setComment: function(value) {
+			$("#nitdoc-github-commentbox-comment" + this._id).val(value);
 		},
 
-		_getArea: function() {
-			return this.commentBox.find("textarea.nitdoc-github-commentarea");
+		_getComment: function() {
+			return $("#nitdoc-github-commentbox-comment" + this._id).val();
 		},
 
-		_setValue: function(value) {
-			this._getArea().val(value);
+		_getMessage: function() {
+			return $("#nitdoc-github-commentbox-message" + this._id).val();
 		},
 
-		_getValue: function() {
-			return this._getArea().val();
+		_setMessage: function(message) {
+			$("#nitdoc-github-commentbox-message" + this._id).val(message);
+		},
+
+		_getSignedOff: function() {
+			return $("#nitdoc-github-commentbox-signedoff" + this._id).val();
+		},
+
+		_setSignedOff: function(signedoff) {
+			$("#nitdoc-github-commentbox-signedoff" + this._id).val(signedoff);
+			$("#nitdoc-github-commentbox-signedoff-label" + this._id).text(signedoff);
 		},
 
 		/* events */
 
-		_doKeyUp: function() {
-			if(this._getValue() == this._originalValue) {
-				this.commentBox.find("button.nitdoc-github-commit").attr("disabled", "disabled");
-			} else {
-				this.commentBox.find("button.nitdoc-github-commit").removeAttr("disabled");
-			}
-			this._autosize();
+		_doKeyUp: function(event) {
+			$(event.target).height($(event.target).val().split(/\r|\n/).length * 16);
 		},
 
 		_doKeyDown: function(event) {
 			if(event.keyCode == 13){
-				this._getArea().css("height", (this._getArea().outerHeight() + 6) + "px");
+				$(event.target).css("height", ($(event.target).outerHeight() + 6) + "px");
+			}
+		},
+
+		_doSignedChange: function(event) {
+			if ($(event.currentTarget).is(':checked')) {
+				this.commentBox.find("button.nitdoc-github-commit").removeAttr("disabled");
+			} else {
+				this.commentBox.find("button.nitdoc-github-commit").attr("disabled", "disabled");
 			}
 		},
 
 		_doPreviewClick: function(event) {
-			this._trigger("_preview", event, {value: this._getValue()});
+			this._trigger("_preview", event, {value: this._getComment()});
 		},
 
 		_doCommitClick: function() {
-			this._trigger("_commit", {value: this._getValue()});
+			this._trigger("_commit", event, {
+				requestID: this._requestID,
+				location: this._location,
+				namespace: this._namespace,
+				oldComment: this._oldComment,
+				newComment: this._getComment(),
+				title: this._getMessage(),
+				message: this._getMessage() + "\n\n" + this._getSignedOff()
+			});
 		},
 
 		_doCancelClick: function() {
