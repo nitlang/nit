854c30b8860c2969ecda7758bd2c2d1a6b7f1d72
Alexis Laferrière <alexis.laf@xymus.net>
2014-02-19 14:49:29 -0500

    share/png: add Android specific makefiles to the png library
    Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

---
+++ share/png/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := png
+LOCAL_SRC_FILES :=\
+	png.c \
+	pngerror.c \
+	pngget.c \
+	pngmem.c \
+	pngpread.c \
+	pngread.c \
+	pngrio.c \
+	pngrtran.c \
+	pngrutil.c \
+	pngset.c \
+	pngtrans.c \
+	pngwio.c \
+	pngwrite.c \
+	pngwtran.c \
+	pngwutil.c
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+
+#include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
