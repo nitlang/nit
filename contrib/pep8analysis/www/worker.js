0fc32054ffb33d0fa189883f8454b358d59e5b2c
Alexis Laferrière <alexis.laf@xymus.net>
2014-06-26 14:43:14 -0400

    pep8analysis: do work async using workers
    Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

---
+++ contrib/pep8analysis/www/worker.js
@@ -0,0 +1,27 @@
+// load
+var Module = {
+	'print': function(text) {
+		postMessage({'type': 'stdout', 'data': text})
+	},
+	'TOTAL_MEMORY': 512000000
+}
+importScripts("pep8analysis.js")
+run_analysis = Module.cwrap('pep8analysis_web___NativeString_run_analysis', null, ['string'])
+
+function show_graph(data) {
+	postMessage({'type':"show_graph", 'data': data})
+}
+
+// notify UI it's ready
+postMessage({'type':"ready"})
+
+// wait for code to analyze
+self.onmessage = function(event){
+	try {
+		run_analysis(event.data)
+		postMessage({'type':"complete"})
+	} catch(e) {
+		console.log(e)
+		postMessage({'type':"exception", 'data': e.toString()})
+	}
+}
