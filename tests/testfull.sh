99397380fd0494f327ea2ba1b30660718de90889
Jean Privat <jean@pryen.org>
2014-08-27 23:33:53 -0400

    tests: split testfull.sh into listfull.sh that just lists testfiles
    Signed-off-by: Jean Privat <jean@pryen.org>

--- tests/testfull.sh
+++ tests/testfull.sh
@@ -1,22 +1,2 @@
 #!/bin/sh
-list=""
-for x in ../lib/*
-do
-	bx=`basename $x`
-	y="$x/$bx.nit"
-	test -f $y && list="$list $y"
-done
-./tests.sh "$@" *.nit \
-	../examples/*.nit \
-	../examples/*/*.nit \
-	../examples/shoot/src/shoot_logic.nit \
-	../examples/*/src/*_android.nit \
-	../examples/*/src/*_linux.nit \
-	../examples/*/src/*_null.nit \
-	../examples/pnacl/converter/converter.nit \
-	../lib/*.nit $list \
-	../src/nit*.nit \
-	../src/test_*.nit \
-	../contrib/*.nit \
-	../contrib/*/*.nit \
-	../contrib/*/src/*.nit
+./tests.sh "$@" `./listfull.sh`
