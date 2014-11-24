1a64d64954b3749e0899ac413fa0d11e077a0735
Jean-Christophe Beaupré <jcbrinfo@users.noreply.github.com>
2014-11-24 09:39:24 -0500

    neo_doxygen: Register tests.
    Signed-off-by: Jean-Christophe Beaupré <jcbrinfo@users.noreply.github.com>

--- tests/listfull.sh
+++ tests/listfull.sh
@@ -13,5 +13,6 @@ printf "%s\n" "$@" \
 	../examples/mpi/src/*.nit \
 	../lib/*/examples/*.nit \
 	../contrib/friendz/src/solver_cmd.nit \
+	../contrib/neo_doxygen/src/tests/neo_doxygen_*.nit \
 	../contrib/pep8analysis/src/pep8analysis.nit \
 	*.nit
