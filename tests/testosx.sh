16c5104bca718579f2aafe0d41c76e630e0088e2
Alexis Laferrière <alexis.laf@xymus.net>
2015-03-31 13:58:29 -0400

    tests: add the testosx.sh script to run OSX and iOS specific tests
    Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

---
+++ tests/testosx.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This file is part of NIT ( http://www.nitlanguage.org ).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Tests to run on OS X for the host platform and iOS
+./tests.sh $@ \
+	test_ffi_objc_*.nit \
+	test_*ios.nit \
+	../lib/ios/examples/*.nit \
+	../lib/cocoa*/examples/*.nit
