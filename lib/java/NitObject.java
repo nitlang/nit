f2aedb0dfeb69bbe3d0bb1abe8df376db1ae8fbc
Alexis Laferrière <alexis.laf@xymus.net>
2019-02-17 13:11:17 -0500

    Java FFI: intro NitObject for references to Nit objects from Java
    Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

---
+++ lib/java/NitObject.java
@@ -0,0 +1,27 @@
+/* This file is part of NIT ( http://www.nitlanguage.org ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	 http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nit.app;
+
+// General class for all references to Nit objects from Java in the FFI
+public class NitObject {
+
+	// Address to the object in Nit memory
+	private long pointer;
+
+	protected NitObject(long pointer) {
+		this.pointer = pointer;
+	}
+}
