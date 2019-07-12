nitlang/nit

commit 539ca148d6164805d5aa78e696095336dac11ae5

nitc & niti: move the duplicated pkg-config checks to the pkgconfig module
Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

src/compiler/abstract_compiler.nit
@@ -24,6 +24,7 @@ import c_tools
 private import annotation
 import mixin
 import counter
+import pkgconfig
 
 # Add compiling options
 redef class ToolContext
@@ -187,6 +188,8 @@ class MakefileToolchain
 
 		var time1 = get_time
 		self.toolcontext.info("*** END WRITING C: {time1-time0} ***", 2)
+
+		if not toolcontext.check_errors then return
 
 		# Execute the Makefile
 
@@ -466,14 +469,19 @@ endif
 			f.close
 		end
 
-		var java_files = new Array[ExternFile]
-
+		# pkg-config annotation support
 		var pkgconfigs = new Array[String]
 		for f in compiler.extern_bodies do
 			pkgconfigs.add_all f.pkgconfigs
 		end
-		# Protect pkg-config
+
+		# Only test if pkg-config is used
 		if not pkgconfigs.is_empty then
+
+			# Check availability of pkg-config, silence the proc output
+			toolcontext.check_pkgconfig_packages pkgconfigs
+
+			# Double the check in the Makefile in case it's distributed
 			makefile.write """
 # does pkg-config exists?
 ifneq ($(shell which pkg-config >/dev/null; echo $$?), 0)
@@ -491,6 +499,7 @@ endif
 		end
 
 		# Compile each required extern body into a specific .o
+		var java_files = new Array[ExternFile]
 		for f in compiler.extern_bodies do
 			var o = f.makefile_rule_name
 			var ff = f.filename.basename

src/ffi/pkgconfig.nit
@@ -25,6 +25,42 @@ private import literal
 redef class ToolContext
 	# Detects the `pkgconfig` annotation on the module declaration only
 	var pkgconfig_phase: Phase = new PkgconfigPhase(self, [literal_phase])
+
+	# Is the external program `pkg-config` available?
+	var pkgconfig_is_available: Bool is lazy do
+		# Ignore/silence the process output
+		var proc_which = new ProcessReader("which", "pkg-config")
+		proc_which.wait
+
+		var status = proc_which.status
+		if status != 0 then
+			error(null, "Error: program `pkg-config` not found, make sure it is installed.")
+			return false
+		end
+		return true
+	end
+
+	# Check if the `packages` are known by the external program `pkg-config`
+	#
+	# Missing packages are reported to the console via `ToolContext::error`.
+	# Check for errors using `check_errors`.
+	fun check_pkgconfig_packages(packages: Array[String])
+	do
+		if not pkgconfig_is_available then return
+
+		for pkg in packages do
+			var proc_exist = new Process("pkg-config", "--exists", pkg)
+			proc_exist.wait
+			var status = proc_exist.status
+			if status == 1 then
+				error(null,
+					"Error: dev package for `{pkg}` unknown by `pkg-config`, install it with `apt-get`, `brew` or similar.")
+			else if status != 0 then
+				error(null,
+					"Error: something went wrong calling `pkg-config`, make sure it is correctly configured.")
+			end
+		end
+	end
 end
 
 # Detects the `pkgconfig` annotation on the module declaration only
@@ -67,27 +103,7 @@ private class PkgconfigPhase
 			end
 		end
 
-		# check availability of pkg-config
-		var proc_which = new ProcessReader("which", "pkg-config")
-		proc_which.wait
-		var status = proc_which.status
-		if status != 0 then
-			modelbuilder.error(nat, "Error: program `pkg-config` not found, make sure it is installed.")
-			return
-		end
-
 		for pkg in pkgs do
-			var proc_exist = new Process("pkg-config", "--exists", pkg)
-			proc_exist.wait
-			status = proc_exist.status
-			if status == 1 then
-				modelbuilder.error(nat, "Error: dev package for `{pkg}` unknown by `pkg-config`, install it with `apt-get`, `brew` or similar.")
-				continue
-			else if status != 0 then
-				modelbuilder.error(nat, "Error: something went wrong calling `pkg-config`, make sure it is correctly installed.")
-				continue
-			end
-
 			mmodule.pkgconfigs.add pkg
 		end
 	end

src/interpreter/dynamic_loading_ffi/on_demand_compiler.nit
@@ -138,19 +138,10 @@ redef class AModule
 		var pkgconfigs = mmodule.pkgconfigs
 		var pkg_cflags = ""
 		if not pkgconfigs.is_empty then
-			var cmd = "which pkg-config >/dev/null"
-			if system(cmd) != 0 then
-				v.fatal "FFI Error: Command `pkg-config` not found. Please install it"
-				return false
-			end
 
-			for p in pkgconfigs do
-				cmd = "pkg-config --exists '{p}'"
-				if system(cmd) != 0 then
-					v.fatal "FFI Error: package {p} is not found by `pkg-config`. Please install it."
-					return false
-				end
-			end
+			# Check if the pkgconfig packages are available
+			v.modelbuilder.toolcontext.check_pkgconfig_packages pkgconfigs
+			if not v.modelbuilder.toolcontext.check_errors then return false
 
 			pkg_cflags = "`pkg-config --cflags {pkgconfigs.join(" ")}`"
 			ldflags += " `pkg-config --libs {pkgconfigs.join(" ")}`"

tests/sav/error_annot_pkgconfig_alt0.res
@@ -1,1 +1,1 @@
-alt/error_annot_pkgconfig_alt0.nit:17,38--46: Error: dev package for `error_annot_pkgconfig_alt0` unknown by `pkg-config`, install it with `apt-get`, `brew` or similar.
+Error: dev package for `error_annot_pkgconfig_alt0` unknown by `pkg-config`, install it with `apt-get`, `brew` or similar.

tests/sav/error_annot_pkgconfig_alt1.res
@@ -1,2 +1,2 @@
-alt/error_annot_pkgconfig_alt1.nit:18,38--82: Error: dev package for `missing-lib` unknown by `pkg-config`, install it with `apt-get`, `brew` or similar.
-alt/error_annot_pkgconfig_alt1.nit:18,38--82: Error: dev package for `other missing lib` unknown by `pkg-config`, install it with `apt-get`, `brew` or similar.
+Error: dev package for `missing-lib` unknown by `pkg-config`, install it with `apt-get`, `brew` or similar.
+Error: dev package for `other missing lib` unknown by `pkg-config`, install it with `apt-get`, `brew` or similar.

commit 1a4ef6f94724d8c543290da053047f60334ac3f6

nitc: fix --keep-going in the compiler broken by 539ca148d
Signed-off-by: Jean Privat <jean@pryen.org>

src/compiler/abstract_compiler.nit
@@ -196,7 +196,7 @@ class MakefileToolchain
 		var time1 = get_time
 		self.toolcontext.info("*** END WRITING C: {time1-time0} ***", 2)
 
-		if not toolcontext.check_errors then return
+		toolcontext.check_errors
 
 		# Execute the Makefile
 

tests/nit.args
@@ -6,3 +6,4 @@ base_simple3.nit
 -n -e 'print line' test_prog/README.md test_prog/test_prog.nit
 test_ffi_c_interpreter.nit
 -
+--keep-going test_keep_going.nit --run --dir out/

tests/nitc.args
@@ -9,3 +9,4 @@ test_define.nit -D text=hello -D num=42 -D flag --dir out/ ; out/test_define
 --log --log-dir $WRITE test_prog -o out/test_prog.bin
 test_define.nit --semi-global -D text=hello -D num=42 -D flag --dir out/ ; out/test_define
 --run ../examples/print_arguments.nit 1 2 3 --dir out/
+--keep-going test_keep_going.nit --run --dir out/

tests/niti.skip
@@ -5,6 +5,7 @@ nit_args4
 nit_args5
 nit_args6
 nit_args8
+nit_args9
 nitvm_args1
 nitvm_args3
 nitin

tests/nitvm.skip
@@ -5,6 +5,7 @@ nit_args4
 nit_args5
 nit_args6
 nit_args8
+nit_args9
 nitvm_args1
 nitvm_args3
 nitin

tests/sav/nit_args9.res
@@ -1,0 +1,25 @@
+[0;33mtest_keep_going.nit:15,11--14[0m: Error: class `Fail` not found in module `test_keep_going`.
+	fun plop: [1;31mFail[0m
+	          ^
+[0;33mtest_keep_going.nit:40,2--11[0m: Error: unreachable statement.
+		[1;31m999.output[0m
+		^
+[0;33mtest_keep_going.nit:26,2--5[0m: Error: method or variable `fail` unknown in `Sys`.
+		[1;31mfail[0m
+		^
+[0;33mtest_keep_going.nit:30,14--17[0m: Error: class `Fail` not found in module `test_keep_going`.
+		var x = new [1;31mFail[0m
+		            ^
+[0;33mtest_keep_going.nit:35,5[0m: Type Error: expected `Bool`, got `Int`.
+		if [1;31m1[0m then abort
+		   ^
+[0;33mtest_keep_going.nit:44,18--21[0m: Error: method `fail` does not exists in `Sys`.
+		var a = new Sys.[1;31mfail[0m
+		                ^
+1
+2
+3
+4
+5
+6
+7

tests/sav/nitc_args12.res
@@ -1,0 +1,26 @@
+[0;33mtest_keep_going.nit:15,11--14[0m: Error: class `Fail` not found in module `test_keep_going`.
+	fun plop: [1;31mFail[0m
+	          ^
+[0;33mtest_keep_going.nit:40,2--11[0m: Error: unreachable statement.
+		[1;31m999.output[0m
+		^
+[0;33mtest_keep_going.nit:26,2--5[0m: Error: method or variable `fail` unknown in `Sys`.
+		[1;31mfail[0m
+		^
+[0;33mtest_keep_going.nit:30,14--17[0m: Error: class `Fail` not found in module `test_keep_going`.
+		var x = new [1;31mFail[0m
+		            ^
+[0;33mtest_keep_going.nit:35,5[0m: Type Error: expected `Bool`, got `Int`.
+		if [1;31m1[0m then abort
+		   ^
+[0;33mtest_keep_going.nit:44,18--21[0m: Error: method `fail` does not exists in `Sys`.
+		var a = new Sys.[1;31mfail[0m
+		                ^
+Errors: 6. Warnings: 0.
+1
+2
+3
+4
+5
+6
+7